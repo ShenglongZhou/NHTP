a145eb925a74df6607c75de0f5932b725e8f57d3
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2019-12-06 15:56:00 +0000

    Update lcp_data.m

--- NHTP/examples/complementarity_problem/lcp/lcp_data.m
+++ NHTP/examples/complementarity_problem/lcp/lcp_data.m
@@ -1,4 +1,23 @@
 function data = lcp_data(example,n,s )
+% This file aims at generating data of 3 examples, Gaussian, Partial DCT, 
+% and Toeplitz Correlation type measurement matrices.
+% Inputs:
+%       example     -- can be 'z-mat','sdp' or 'sdp-non'
+%       n           -- dimension  of M\in \R^{n x n}
+%       s           -- sparsity level of xopt, an intger between 1 and n-1
+% Outputs:
+%       data.M      --  n x n order measurement matrices,  (required)
+%       data.Mt     --  the transpose of data.M, i.e., data.Mt=data.M', (required)
+%       data.q      --  n x 1 order observation vector, (required)
+%       data.n      -- dimension  of M\in \R^{n x n}
+%       data.xopt   --  n x 1 order 'true' sparse solution, (optional) 
+%
+% which satisfies       b = A*x_opt + nf*noise 
+
+% written by Shenglong Zhou, 05/12/2019
+
+start = tic;
+fprintf(' Start to generate the linear complementarity problem data...\n'); 
 switch example
     case 'z-mat'
          M       = eye(n)-ones(n)/n;
@@ -32,6 +51,8 @@ data.Mt   = Mt;
 data.q    = q;
 data.n    = n;
 data.xopt = xopt;
+
+fprintf(' Data generation used %2.4f seconds.\n\n',toc(start)); 
 end
 
 
