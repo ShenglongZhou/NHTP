dccef456f55b46a826cf4aac1ec88128c357cdf8
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2019-12-06 15:58:12 +0000

    Update logistic_random_data.m

--- NHTP/examples/logistic_regression/logistic_random_data.m
+++ NHTP/examples/logistic_regression/logistic_random_data.m
@@ -16,7 +16,7 @@ function [data,out] = logistic_random_data(type,m,n,s,rho)
 % written by Shenglong Zhou, 13/10/2018
 
 start = tic;
-fprintf('Start to generate the logistic regression data...\n'); 
+fprintf(' Start to generate the logistic regression data...\n'); 
 switch type
     case 'Indipendent'
         I0    = randperm(m);
