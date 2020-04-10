089001328eccc8e2bc1364a090de6efd86cfb280
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2020-04-10 19:49:14 +0100

    Update demon_SLR_RealData.m

--- NHTP/demon_SLR_RealData.m
+++ NHTP/demon_SLR_RealData.m
@@ -2,20 +2,21 @@
 clc; close all; clear;
 
 prob      = 'newsgroup'; %'colon-cancer'
-measure   = load(strcat(prob,'.mat')); 
+Mat       = load(strcat(prob,'.mat')); 
 label     = load(strcat(prob,'_label.mat'));   
 label.b(label.b==-1)= 0;
-[m,n]     = size(measure.A);  
-data.A    = normalization(measure.A,1+(m>= 1000)); 
+[m,n]     = size(Mat.A);  
+data.A    = normalization(Mat.A,1+(m>= 1000)); 
 data.At   = data.A';
 data.b    = label.b; 
-clear measure label;
+clear Mat label;
 
-s         = ceil(0.01*m);
-pars.eta  = 1;
-func      = @(x,fgh,T1,T2)logistic_regression(x,fgh,T1,T2,data); clear data
-out       = NHTP(n,s,func);
+s         = ceil(0.1*m);
+pars.eta  = 5;
+fname     = str2func('logistic_regression');
+func      = @(x,fgh,T1,T2)fname(x,fgh,T1,T2,data); clear data
+out       = NHTP(n,s,func) 
 
-saveas(figure(1), [pwd strcat(strcat('/outputs/',prob))]);  
-saveas(figure(1), [pwd strcat(strcat('/outputs/',prob),'.eps')],'epsc');
-
+path      = strcat(strcat('/outputs/',prob));
+saveas(figure(1), [pwd path]);  
+saveas(figure(1), [pwd path,'.eps'],'epsc');
