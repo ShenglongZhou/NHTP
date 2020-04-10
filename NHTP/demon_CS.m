cb1dd75dffcc7eceb657fb5544321113a4e597a5
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2020-04-10 19:28:17 +0100

    Update demon_CS.m

--- NHTP/demon_CS.m
+++ NHTP/demon_CS.m
@@ -6,18 +6,18 @@ m       = ceil(n/4);
 s       = ceil(0.01*n);                      
  
 % % You could input any data including (data.A, data.At, data.b) 
-% data.A    = randn(m,n)/sqrt(m);
-% data.At   = data.A';
-% data.b    = randn(m,1)/sqrt(m);  
+% data.A   = randn(m,n)/sqrt(m);
+% data.At  = data.A';
+% data.b     randn(m,1)/sqrt(m);  
 
 % Or you could input data from our data generation function
- ExMat = 1;
- MatType = {'GaussianMat','PartialDCTMat'}; 
- data    = compressed_sensing_data(MatType{ExMat}, m,n,s,0);
+ExMat    = 1;
+MatType  = {'GaussianMat','PartialDCTMat'}; 
+data     = compressed_sensing_data(MatType{ExMat}, m,n,s,0);
 
-pars.eta  = 1;
-func      = @(x,fgh,T1,T2)compressed_sensing(x,fgh,T1,T2,data);
-out       = NHTP(n,s,func,pars) 
+pars.eta = 1;
+func     = @(x,fgh,T1,T2)compressed_sensing(x,fgh,T1,T2,data); clear data
+out      = NHTP(n,s,func,pars) 
 
 fprintf('\n Sample size:       m=%d,n=%d\n', m,n);
 fprintf(' Recovery time:    %6.3fsec\n',  out.time);
