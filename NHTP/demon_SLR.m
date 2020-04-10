0a6dcca9c8f6cb03d6ce6b5cb0d0c08f54e24709
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2020-04-10 19:48:38 +0100

    Update demon_SLR.m

--- NHTP/demon_SLR.m
+++ NHTP/demon_SLR.m
@@ -4,25 +4,25 @@ clc; clear; close all;
 n    = 2000;  
 m    = ceil(n/5); 
 s    = ceil(0.05*n);
-                      
-% % You could input any data including (data.A, data.At, data.b) 
-% I0      = randperm(n); 
-% I       = I0(1:s);
-% x       = zeros(n,1);
-% x(I)    = randn(s,1);
-% data.A  = randn(m,n); 
-% data.At = data.A';
-% q       = 1./(1+exp(-data.A(:,I)*x(I)));
-% data.b  = zeros(m,1);
-% for i   = 1:m; data.b(i) = randsrc(1,1,[0 1; 1-q(i) q(i)]); end 
-
-% Or you could input data from our data generation function 
-ExMat    = 1;
-MatType  = {'Indipendent','Correlated'};
-data     = logistic_random_data(MatType{ExMat},m,n,s,0.5); 
+test = 2;                     
+switch test
+    case 1  % You could input any data including (data.A, data.At, data.b), e.g, 
+         I0      = randperm(m);  
+         I       = I0(1:ceil(m/2)); 
+         b       = ones(m,1);    
+         b(I)    = 0;
+         data.A  = repmat(b.*rand(m,1),1,n) + randn(m,n);
+         data.At = data.A';    
+         data.b  = b;
+    case 2  % Or you could input data by our data generation function 
+         ExMat   = 2;
+         MatType = {'Correlated','Weakly-Indipendent'};
+         data    = logistic_random_data(MatType{ExMat},m,n,s,0.5); 
+end
 
 pars.eta = 5;
-func     = @(x,fgh,T1,T2)logistic_regression(x,fgh,T1,T2,data);
+fname    = str2func('logistic_regression');
+func     = @(x,fgh,T1,T2)fname(x,fgh,T1,T2,data); clear data
 out      = NHTP(n,s,func); 
 
 fprintf('\n Sample size:   m=%4d,n=%4d\n', m,n);
