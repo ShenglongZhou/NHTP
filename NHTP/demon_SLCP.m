7d5b3d7f3986cf93e81eb63f3dbe7a33ce52f592
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2020-04-25 15:34:26 +0100

    Update demon_SLCP.m

--- NHTP/demon_SLCP.m
+++ NHTP/demon_SLCP.m
@@ -13,7 +13,11 @@ fun        = @(x,T)(data.M(:,T)*x(T)+data.q);
 out0       = HTPCP(n, fun , pars0)  
 
 % Second phase
-pars.x0  = out0.x;
+if  exist('out0 ')
+    pars.x0  = out0.x;
+else
+    pars.x0 = zeros(n,1);
+end
 func     = @(x,fgh,T1,T2)slcp(x,fgh,T1,T2,data);
 out      = NHTP(n,s,func,pars)  
 
