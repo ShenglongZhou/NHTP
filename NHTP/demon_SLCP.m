3ef2416f5127213075d7fdc30da1694b87d34f45
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2020-06-18 16:43:50 +0100

    Update demon_SLCP.m

--- NHTP/demon_SLCP.m
+++ NHTP/demon_SLCP.m
@@ -7,10 +7,10 @@ ExMat   = 2; %= 1, 2, 3
 MatType  = {'z-mat','sdp','sdp-non'};
 data     = lcp_data(MatType{ExMat},n,s);
 
-% First phase
+% First phase (one can remove this phase)
 pars0.tol  = 1e-3;
 fun        = @(x,T)(data.M(:,T)*x(T)+data.q);
-out0       = HTPCP(n, fun , pars0)  
+out0       = HTPCP(n, fun , pars0);  
 
 % Second phase
 if  exist('out0')
@@ -19,6 +19,13 @@ else
     pars.x0 = zeros(n,1);
 end
 func     = @(x,fgh,T1,T2)slcp(x,fgh,T1,T2,data);
-out      = NHTP(n,s,func,pars)  
+out      = NHTP(n,s,func,pars);  
 
  
+fprintf(' CPU time:          %.3fsec\n',  out.time);
+if isfield(data,'xopt')
+fprintf(' Accuracy:          %5.2e\n',...
+          norm(out.sol-data.xopt)/norm(data.xopt));
+end
+fprintf(' Objective:         %5.2e\n',  out.obj);
+fprintf(' Sample size:       %dx%d\n', n,n);
