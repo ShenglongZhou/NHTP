46fd23c4a741cd780fdd390fb182e17978f9f9c0
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2020-06-18 16:44:09 +0100

    Update demon_SLR.m

--- NHTP/demon_SLR.m
+++ NHTP/demon_SLR.m
@@ -1,10 +1,11 @@
-% demon sparse logistic regression problems with randomly generated data
+% demon sparse logistic regression problems 
 clc; clear; close all;
 
 n    = 10000;  
 m    = ceil(n/5); 
 s    = ceil(0.05*n);
-test = 2;                     
+test = 1;
+
 switch test
     case 1  % You could input any data including (data.A, data.At, data.b), e.g, 
          I0      = randperm(m);  
@@ -18,9 +19,19 @@ switch test
          ExMat   = 2;
          MatType = {'Correlated','Weakly-Indipendent'};
          data    = logistic_random_data(MatType{ExMat},m,n,s,0.5); 
+    case 3  % Or you could input real data including (data.A, data.At, data.b), e.g,
+         prob      = 'newsgroup'; %'colon-cancer'
+         Mat       = load(strcat(prob,'.mat')); 
+         label     = load(strcat(prob,'_label.mat'));   
+         label.b(label.b==-1)= 0;
+         [m,n]     = size(Mat.A);  
+         s         = ceil(0.2*m);
+         data.A    = normalization(Mat.A,1+(m>= 1000)); 
+         data.At   = data.A';
+         data.b    = label.b; 
+         clear Mat label;
 end
 
-pars.eta = 5;
 fname    = str2func('logistic_regression');
 func     = @(x,fgh,T1,T2)fname(x,fgh,T1,T2,data); clear data
 out      = NHTP(n,s,func); 
