a6c686198256eaaec040fcae455c252f73794bb5
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2020-11-30 20:55:20 +0000

    Update NHTP.m

--- NHTP/NHTP/NHTP.m
+++ NHTP/NHTP/NHTP.m
@@ -245,7 +245,7 @@ end
 
 end
 
-
+% initialize parameters ---------------------------------------------------
 function [x0,obj,g,eta]=getparameters(n,s,x0,func,pars)
 
     if isfield(pars,'x0') && norm(x0)>0
@@ -303,4 +303,14 @@ function [x0,obj,g,eta]=getparameters(n,s,x0,func,pars)
     
 end
 
+% plot the graph: iter v.s. obj (or error) --------------------------------
+function  PlotFun(input,iter,c, key) 
+    if  input(iter)>1e-40 && input(iter)<1e-5
+        semilogy(1:iter,input(1:iter),c,'MarkerSize',7,'LineWidth',1);
+    else
+        plot(1:iter,input(1:iter),c,'MarkerSize',7,'LineWidth',1);
+    end
+    xlabel('Iter'); ylabel(key); grid on    
+    
+end
 
