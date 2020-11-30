75d3aff29439f520d947735cf2fd0147d139c3c1
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2020-11-30 20:54:14 +0000

    Update demon_SLCP.m

--- NHTP/demon_SLCP.m
+++ NHTP/demon_SLCP.m
@@ -25,5 +25,5 @@ fprintf(' CPU time:          %.3fsec\n',  out.time);
 fprintf(' Objective:         %5.2e\n',  out.obj);
 fprintf(' Sample size:       %dx%d\n', n,n);
 if isfield(data,'xopt') && s<=100
-   ReoveryShow(data.xopt,out.sol,[1000, 550, 400 200],1),pause(0.05)
+   ReoveryShow(data.xopt,out.sol,[1000, 550, 400 200],1)
 end
