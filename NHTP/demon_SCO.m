58001fe39b4ca444062d9d0963d26d6110392a29
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2020-11-30 19:32:19 +0000

    Update demon_SCO.m

--- NHTP/demon_SCO.m
+++ NHTP/demon_SCO.m
@@ -8,17 +8,12 @@ n    = 2;
 s    = 1;
 
 % you can find this function in 'examples'-->'general_sco'
-pars.eta  = .1;
+pars.eta  = 0.1;
 data      = @(x,fgh)simple_ex4_func(x,fgh);  
 func      = @(x,fgh,T1,T2)general_example(x,fgh,T1,T2,data);
 out1      = NHTP(n,s,func,pars)  
 
 % or you can solve by following way, 
 % where @exfunc can be found in 'examples'-->'general_sco'
-pars.eta  = .1;
+pars.eta  = 0.1;
 out2      = NHTP(n,s,@exfunc,pars) 
-
-if isfield(pars,'draw') && pars.draw
-saveas(figure(1), 'outputs\GenSCO.eps','epsc');
-saveas(figure(1), 'outputs\GenSCO.fig');
-end
