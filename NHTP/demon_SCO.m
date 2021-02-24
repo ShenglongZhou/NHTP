33fc6dbae85320873a9314b00e3f4b8d3e39b518
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-02-24 23:07:30 +0000

    Update demon_SCO.m

--- NHTP/demon_SCO.m
+++ NHTP/demon_SCO.m
@@ -2,18 +2,9 @@
 %     min    x'*[6 5;5 8]*x+[1 9]*x-sqrt(x'*x+1)  
 %     s.t. \|x\|_0<=s
 % where s=1
+% you can find this function in 'examples'-->'general_sco'
 
 clc; clear; close all;
-n    = 2;    
-s    = 1;
-
-% you can find this function in 'examples'-->'general_sco'
 pars.eta  = 0.1;
-data      = @(x,fgh)simple_ex4_func(x,fgh);  
-func      = @(x,fgh,T1,T2)general_example(x,fgh,T1,T2,data);
-out1      = NHTP(n,s,func,pars)  
-
-% or you can solve by following way, 
-% where @exfunc can be found in 'examples'-->'general_sco'
-pars.eta  = 0.1;
-out2      = NHTP(n,s,@exfunc,pars) 
+data      = @(x,fgh)simple_ex(x,fgh);  
+out1      = NHTP('SCO',data,2,1,pars);
