c6346f7be20b89fd2763b44f5f99bb5aed2f3d1f
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-02-24 23:08:09 +0000

    Update demon_SLR.m

--- NHTP/demon_SLR.m
+++ NHTP/demon_SLR.m
@@ -1,26 +1,18 @@
 % demon sparse logistic regression problems 
 clc; clear; close all;
 
-n    = 10000;  
-m    = ceil(n/5); 
-s    = ceil(0.05*n);
-test = 3;
+test = 2;
 
 switch test
-    case 1  % You could input any data including (data.A, data.At, data.b), e.g, 
-         I0      = randperm(m);  
-         I       = I0(1:ceil(m/2)); 
-         b       = ones(m,1);    
-         b(I)    = 0;
-         data.A  = repmat(b.*rand(m,1),1,n) + randn(m,n);
-         data.At = data.A';    
-         data.b  = b;
-    case 2  % Or you could input data by our data generation function 
+    case 1  % Or you could input data by our data generation function 
          ExMat   = 2;
          MatType = {'Correlated','Weakly-Indipendent'};
+         n       = 10000;  
+         m       = ceil(n/5); 
+         s       = ceil(0.05*n);
          data    = logistic_random_data(MatType{ExMat},m,n,s,0.5); 
-    case 3  % Or you could input real data including (data.A, data.At, data.b), e.g,
-         prob      = 'newsgroup'; %'colon-cancer'
+    case 2  % Or you could input real data including (data.A, data.At, data.b), e.g,
+         prob      = 'newsgroup';%'colon-cancer'; 
          Mat       = load(strcat(prob,'.mat')); 
          label     = load(strcat(prob,'_label.mat'));   
          label.b(label.b==-1)= 0;
@@ -32,10 +24,7 @@ switch test
          clear Mat label;
 end
 
-fname    = str2func('logistic_regression');
-func     = @(x,fgh,T1,T2)fname(x,fgh,T1,T2,data); clear data
-out      = NHTP(n,s,func); 
-
+out = NHTP('LR',data,n,s);
 fprintf(' CPU time:      %.3fsec\n',  out.time);
 fprintf(' Logistic Loss: %5.2e\n', out.obj);
 fprintf(' Sample size:   %4dx%4d\n', m,n);
