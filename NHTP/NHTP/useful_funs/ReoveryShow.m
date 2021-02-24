faeb1645c54c549799a00d233843c6e1574ab72e
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-02-24 22:08:58 +0000

    Update ReoveryShow.m

--- NHTP/NHTP/useful_funs/ReoveryShow.m
+++ NHTP/NHTP/useful_funs/ReoveryShow.m
@@ -1,6 +1,7 @@
 function ReoveryShow(xo,x,pos,ind)
 
     figure('Renderer', 'painters', 'Position', pos)
+    axes('Position', [0.05 0.1 0.9 0.8] );
     stem(find(xo),xo(xo~=0),'bo-','MarkerSize',6, 'LineWidth',1),hold on
     stem(find(x),x(x~=0),'r*:', 'MarkerSize',4, 'LineWidth',1),hold on
     grid on, ymin= -0.1; ymax=0.2;
