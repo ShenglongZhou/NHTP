f37dd7bffc9852fcd276bb8abfc55b0066909a52
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-02-24 23:12:04 +0000

    Update NHTP.m

--- NHTP/NHTP/NHTP.m
+++ NHTP/NHTP/NHTP.m
@@ -12,7 +12,7 @@ function Out = NHTP(problem,data,n,s,pars)
 %               = 'LCP', linear complementarity problems
 %               = 'LR',  sparse logistic regression problems
 %               = 'SCO', other sparsity constrained optimization problems
-%     data    : A triple structure, (required)
+%     data    : A triple structure (data.A, data.At, data.b) (required)
 %               data.A, the measurement matrix, or a function handle @(x)A(x);
 %               data.At = data.A',or a function handle @(x)At(x);
 %               data.b, the observation vector 
