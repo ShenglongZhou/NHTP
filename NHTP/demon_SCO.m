f4df9c163fe4e337d25e196fb366429e9e6923d5
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-03-03 14:58:54 +0000

    Update demon_SCO.m

--- NHTP/demon_SCO.m
+++ NHTP/demon_SCO.m
@@ -1,10 +1,28 @@
+function demon_sco()
 % demon a general sparsity constrained problem
-%     min    x'*[6 5;5 8]*x+[1 9]*x-sqrt(x'*x+1)  
-%     s.t. \|x\|_0<=s
-% where s=1
-% you can find this function in 'examples'-->'general_sco'
 
 clc; clear; close all;
 pars.eta  = 0.1;
-data      = @(x,fgh)simple_ex(x,fgh);  
-out1      = NHTP('SCO',data,2,1,pars);
+n         = 2;
+s         = 1;
+data      = @(x,fgh)simple_sco(x,fgh);  
+out       = NHTP('SCO',data,n,s,pars);
+
+%-----------------------------------------------------
+function data = simple_sco(x,fgH)
+    % This code provides information for
+    %     min   x'*[6 5;5 8]*x+[1 9]*x-sqrt(x'*x+1) 
+    %     s.t. \|x\|_0<=s
+    % where s=1
+    a = sqrt(sum(x.*x)+1);
+    switch fgH    
+        case 'obj'  % objective function
+        data = x'*[6 5;5 8]*x+[1 9]*x-a;        
+        case 'grad'  % gradient 
+        data = 2*[6 5;5 8]*x+[1; 9]-x./a;        
+        case 'hess'  % Hessian matrix 
+        data = 2*[6 5;5 8]+(x*x'-a*eye(2))/a^3;  
+    end   
+end
+
+end
