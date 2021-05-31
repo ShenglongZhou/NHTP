1197ba8a0cc29416585d63d0329e989e19c1853f
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-05-31 22:12:50 +0100

    Update demon_CS.m

--- NHTP/demon_CS.m
+++ NHTP/demon_CS.m
@@ -3,15 +3,15 @@ clc; clear; close all;
 
 n     = 10000;  
 m     = ceil(n/4); 
-s     = ceil(0.01*n);                      
+s     = ceil(0.05*n);                      
 
-test  = 2; 
+test  = 1; 
 
 switch test
   case 1       % Input data by our data generation function
        ExMat    = 1;
        MatType  = {'GaussianMat','PartialDCTMat'}; 
-       nf       = 0;
+       nf       = 0.00;
        data     = CSdata(MatType{ExMat},m,n,s,nf);
        xopt     = data.xopt;
        if nf    > 0; pars.eta = 0.5; end
@@ -23,9 +23,9 @@ switch test
        data.A   = randn(m,n)/sqrt(m);
        data.b   = data.A*xopt;  
 end  
- 
+pars.tol  = 1e-6; 
 func      = @(x,T1,T2)CS(x,T1,T2,data);
-out       = NHTP(func,n,s);  
+out       = NHTP(func,n,s,pars);  
 fprintf(' CPU time:          %.3fsec\n',  out.time);
 fprintf(' Objective:         %5.2e\n',  out.obj);
 fprintf(' Sample size:       %dx%d\n', m,n);
