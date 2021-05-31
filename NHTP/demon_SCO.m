0f4a3eea1726c4f84fc3e0086b2c9392785f1403
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-05-31 21:55:33 +0100

    Update demon_SCO.m

--- NHTP/demon_SCO.m
+++ NHTP/demon_SCO.m
@@ -1,26 +1,31 @@
-% demon a general sparsity constrained problem
+5% demon a general sparsity constrained problem
 function demon_sco()
 clc; clear; close all;
 pars.eta  = 0.1;
 n         = 2;
-s         = 1;
-data      = @(x,fgh)simple_sco(x,fgh);  
-out       = NHTP('SCO',data,n,s,pars);
+s         = 1; 
+out       = NHTP(@func,n,s,pars);
+fprintf(' CPU time:       %.3fsec\n',  out.time);
+fprintf(' Objective:      %.4f\n', out.obj); 
 
 %---------------------------------------------------------
-function data = simple_sco(x,fgH)
+function [out1,out2] = func(x,T1,T2)
     % This code provides information for
     %     min   x'*[6 5;5 8]*x+[1 9]*x-sqrt(x'*x+1) 
     %     s.t. \|x\|_0<=s
     % where s=1
-    a = sqrt(sum(x.*x)+1);
-    switch fgH    
-        case 'obj'  % objective function
-        data = x'*[6 5;5 8]*x+[1 9]*x-a;        
-        case 'grad'  % gradient 
-        data = 2*[6 5;5 8]*x+[1; 9]-x./a;        
-        case 'hess'  % Hessian matrix 
-        data = 2*[6 5;5 8]+(x*x'-a*eye(2))/a^3;  
+    a   = sqrt(sum(x.*x)+1);
+    if  isempty(T1) && isempty(T2)    
+        out1 = x'*[6 5;5 8]*x+[1 9]*x-a;        
+        if  nargout == 2
+            out2 = 2*[6 5;5 8]*x+[1; 9]-x./a;  
+        end
+    else
+        H    = 2*[6 5;5 8]+(x*x'-a*eye(2))/a^3;  
+        out1 = H(T1,T1);
+        if  nargout == 2
+            out2 = H(T1,T2);  
+        end
     end   
 end
 
