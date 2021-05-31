63b192b08bb8d575179e59284693d4dd09dedc7c
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-05-31 21:55:55 +0100

    Update demon_SLCP.m

--- NHTP/demon_SLCP.m
+++ NHTP/demon_SLCP.m
@@ -1,17 +1,15 @@
-% demon sparse complementarity problems by two phases procedure
+% demon sparse complementarity problems
 clc; clear; close all;                    
 n         = 10000;  
 s         = ceil(0.01*n);
 ExMat     = 2; %= 1, 2, 3
-
 MatType   = {'z-mat','sdp','sdp-non'};
-data      = lcp_data(MatType{ExMat},n,s);
-pars.draw = 0; 
-out       = NHTP('LCP',data,n,s,pars);
+data      = LCPdata(MatType{ExMat},n,s);
+func      = @(x,T1,T2)LCP(x,T1,T2,data);
+out       = NHTP(func,n,s);
 
 fprintf(' CPU time:          %.3fsec\n',  out.time);
 fprintf(' Objective:         %5.2e\n',  out.obj);
 fprintf(' Sample size:       %dx%d\n', n,n);
-if isfield(data,'xopt') && s<=100
-   ReoveryShow(data.xopt,out.sol,[1000, 550, 400 200],1)
-end
+ReoveryShow(data.xopt,out.sol,[900,500,500,250],1);
+
