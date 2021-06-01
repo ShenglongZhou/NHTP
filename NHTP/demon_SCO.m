908f8af995a760a0f37be08a221b7996123a3da2
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-06-01 11:01:59 +0100

    Update demon_SCO.m

--- NHTP/demon_SCO.m
+++ NHTP/demon_SCO.m
@@ -16,15 +16,15 @@ function [out1,out2] = func(x,T1,T2)
     % where s=1
     a   = sqrt(sum(x.*x)+1);
     if  isempty(T1) && isempty(T2)    
-        out1 = x'*[6 5;5 8]*x+[1 9]*x-a;        
+        out1 = x'*[6 5;5 8]*x+[1 9]*x-a;        % objective    
         if  nargout == 2
-            out2 = 2*[6 5;5 8]*x+[1; 9]-x./a;  
+            out2 = 2*[6 5;5 8]*x+[1; 9]-x./a;   % gradient
         end
     else
         H    = 2*[6 5;5 8]+(x*x'-a*eye(2))/a^3;  
-        out1 = H(T1,T1);
+        out1 = H(T1,T1);                        % sub-Hessian on (T1 T1) 
         if  nargout == 2
-            out2 = H(T1,T2);  
+            out2 = H(T1,T2);                    % sub-Hessian on (T1 T2) 
         end
     end   
 end
