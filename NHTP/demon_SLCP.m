4a90d69f2d76c4e187720e5282b862c9c2bc46b9
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2023-08-13 15:21:09 +0800

    Update demon_SLCP.m

--- NHTP/demon_SLCP.m
+++ NHTP/demon_SLCP.m
@@ -2,7 +2,7 @@
 clc; clear; close all;                    
 n         = 10000;  
 s         = ceil(0.01*n);
-ExMat     = 2; %= 1, 2, 3
+ExMat     = 3; %= 1, 2, 3
 MatType   = {'z-mat','sdp','sdp-non'};
 data      = LCPdata(MatType{ExMat},n,s);
 func      = @(x,T1,T2)LCP(x,T1,T2,data);
@@ -11,5 +11,6 @@ out       = NHTP(func,n,s);
 fprintf(' CPU time:          %.3fsec\n',  out.time);
 fprintf(' Objective:         %5.2e\n',  out.obj);
 fprintf(' Sample size:       %dx%d\n', n,n);
-RecoverShow(data.xopt,out.sol,[900,500,500,250],1);
-
+if  isfield(data,'xopt')
+    RecoverShow(data.xopt,out.sol,[900,500,500,250],1);
+end
